738ca48b32488d8b85fb9d24bb075e1f6cce3ed7
Tayeb-Ali <Mr.elteb@gmail.com>
2020-02-17 23:59:32 +0200

    design register.page.html

--- src/app/Pages/Auth/register/register.page.ts
+++ src/app/Pages/Auth/register/register.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+    selector: 'app-register',
+    templateUrl: './register.page.html',
+    styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+    registerData: any = [
+        {fullname: ''},
+        {phone: ''},
+        {password: ''},
+        {password_check: ''}
+    ];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    userRegister() {
+        console.log('Work');
+    }
 }
