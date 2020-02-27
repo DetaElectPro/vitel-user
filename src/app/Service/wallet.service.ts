c19edb420d6464bca473ad5d379cace6e548288b
Tayeb-Ali <Mr.elteb@gmail.com>
2020-02-28 00:23:29 +0200

    work on Wallet system

--- src/app/Service/wallet.service.ts
+++ src/app/Service/wallet.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class WalletService {
 
-  constructor() { }
+    Url = 'http://localhost:8000/api/';
+    private myHeaders: any;
+    token = `Bearer ${localStorage.getItem('token')}`;
+
+    constructor(
+        private http: HttpClient
+    ) {
+        this.myHeaders = {
+            headers: new HttpHeaders()
+                .set('Content-Type', 'application/json')
+                .set('Authorization', this.token)
+        };
+    }
 }
