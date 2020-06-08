51e70a03d16eb9f204d458cee5f35159f330dd4d
Tayeb-Ali <Mr.elteb@gmail.com>
2020-06-08 17:35:03 +0100

    new update v 1.0.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform, AlertController } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { FCM } from '@ionic-native/fcm/ngx';
-import { NavigationExtras, Router } from '@angular/router';
+import {Platform, AlertController} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
+import {FCM} from '@ionic-native/fcm/ngx';
+import {NavigationExtras, Router} from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -29,6 +29,7 @@ export class AppComponent {
             // this.statusBar.styleDefault();
             this.statusBar.backgroundColorByHexString('#1dcc9b');
             this.splashScreen.hide();
+            this.checkInternet();
             this.getFCM();
         });
     }
@@ -39,34 +40,24 @@ export class AppComponent {
 
         this.fcm.getToken().then(token => {
             localStorage.setItem('fcm_registration_id', token);
-            //   console.log(token);
         });
 
         this.fcm.onNotification().subscribe(data => {
             console.log(data);
             if (data.wasTapped) {
-                if (data.requestId) {
-                    const navigationExtras: NavigationExtras = {
-                        queryParams: {
-                            latitude: data.latitude,
-                            longitude: data.longitude,
-                            id: data.requestId,
-                        }
-                    };
-                    this.router.navigate([`request-details/${data.requestId}`], navigationExtras);
+                if (data.status === 1) {
+                    this.openRequest(data);
+                }
+                if (data.status === 2) {
+                    this.openEmergency(data);
+                }
+                if (data.status === 3) {
+                    this.openPharmacy(data);
                 }
             } else {
                 console.log('Received in foreground');
-                this.normaleAlert('your received new message', 'Request');
-                if (data.requestId) {
-                    const navigationExtras: NavigationExtras = {
-                        queryParams: {
-                            latitude: data.latitude,
-                            longitude: data.longitude,
-                            id: data.requestId,
-                        }
-                    };
-                    this.router.navigate([`request-details/${data.requestId}`], navigationExtras);
+                if (data.status === 1) {
+                    this.normaleAlert(data, 'message', 'There is a new request');
                 }
             }
         });
@@ -74,6 +65,50 @@ export class AppComponent {
         this.fcm.onTokenRefresh().subscribe(token => {
             console.log(token);
         });
+    }
+
+    openRequest(data) {
+        const navigationExtras: NavigationExtras = {
+            queryParams: {
+                latitude: data.latitude,
+                longitude: data.longitude,
+                id: data.requestId,
+            }
+        };
+        this.router.navigate([`request-details/${data.requestId}`], navigationExtras);
+    }
+
+    openEmergency(data) {
+        this.router.navigate([`emergency-details/${data.requestId}`]);
+    }
+
+    openPharmacy(data) {
+        this.router.navigate([`pharmacy-details/${data.requestId}`]);
+    }
+
+    async normaleAlert(data, messageRes, headerRes) {
+        const alert = await this.alertCrtl.create({
+            header: headerRes,
+            message: messageRes,
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: (blah) => {
+                        console.log('Confirm Cancel: blah');
+                    }
+                }, {
+                    text: 'Okay',
+                    handler: () => {
+                        console.log('Confirm Okay');
+                        this.openRequest(data);
+                    }
+                }
+            ]
+        });
+
+        await alert.present();
     }
 
 
@@ -95,14 +130,5 @@ export class AppComponent {
     }
 
 
-    async normaleAlert(messageRes, headerRes) {
-        const alert = await this.alertCrtl.create({
-            header: headerRes,
-            message: messageRes,
-            buttons: ['OK']
-        });
-
-        await alert.present();
-    }
 }
 
